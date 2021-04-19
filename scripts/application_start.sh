a4119e39d4f22f2fcb56ad4835a9326c22534578
Vivek Kumar <Vivek>
2021-04-19 15:57:51 +0530

    Change appspec

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+cd /home/ubuntu/api/AwsCodeDeplyNetCoreApp/AwsCodeDeplyNetCoreApp
+dotnet publish -c Release
+cd bin/Release/netcoreapp3.1/publish/
+sudo mv * /var/www/api
+systemctl start api.service
