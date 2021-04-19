6a8b529769651184b079558e92ce44aeb1d1cd36
Vivek Kumar <Vivek>
2021-04-19 16:16:27 +0530

    Add Values controller

---
+++ AwsCodeDeplyNetCoreApp/AwsCodeDeplyNetCoreApp/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwsCodeDeplyNetCoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new JsonResult("Hello World!!!") { });
+        }
+    }
+}
