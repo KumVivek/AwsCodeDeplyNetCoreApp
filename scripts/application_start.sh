a2705f5784e677a393d7b675022857a6e3054908
Vivek Kumar <Vivek>
2021-04-19 15:39:09 +0530

    change scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+cd /var/www/api/AwsCodeDeplyNetCoreApp/AwsCodeDeplyNetCoreApp
+dotnet publish -c Release
+cd bin/Release/netcoreapp3.1/publish/
+sudo mv * /var/www/api
 systemctl start api.service
